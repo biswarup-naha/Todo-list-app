fe4102e061e0406b1deccecac29f09a7aeffe9e6
biswarup-naha <jeeeeet6902@gmail.com>
2025-04-06 11:56:14 +0530

    swagger ui integrated

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import { connectDb } from './db/db.connection.js'
 import { routes } from './routes/index.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import { setupSwagger } from './swagger.js'
 
 dotenv.config()
 const app = express()
@@ -12,14 +13,18 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
     credentials: true
 }))
 
 //database connection
 connectDb();
 
+//swagger UI setup
+setupSwagger(app);
+
 //routes
 app.use('/api/v1', routes)
+
 
 export default app;
